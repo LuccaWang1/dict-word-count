95b8275fae230329cc3564b71f48be832546165a
Lucca Wang <luccawang@gmail.com>
2023-12-04 16:58:45 -0700

    added more notes for line 12

--- wordcount.py
+++ wordcount.py
@@ -8,6 +8,7 @@ for line in data:
     #strip then split, left to right in order - take off white space first and then split 
     words = line.rstrip().split(" ")
     for word in words:
+       # word is in brackets, because it is pointing to the dictionary and dictionary do not have indices the same way as lists - instead of referencing an index, it is referencing element that it indirectly assigned to that variable in the for loop
        word_counts[word] = word_counts.get(word, 0) + 1
 
 print(word_counts.items())
